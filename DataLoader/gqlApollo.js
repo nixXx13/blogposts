4a35abe638a6a91b170fe1ba5b4a76c514fcca1c
Nir rose <nirrose@Nirs-MacBook-Pro.local>
2023-01-30 21:59:48 +0200

    dataloader

--- DataLoader/gqlApollo.js
+++ DataLoader/gqlApollo.js
@@ -28,21 +28,13 @@ const realLoader = new DataLoader(async (ids) => {
     return ids.map(id => results[id] || new Error(`No result for ${id}`));
 } );
 
-// (async function test(){
-//    await realLoader.load(1);
-//    await realLoader.load(2);
-//    console.log("zzzz");
-//    await cloneLoader.load(1);
-//    await cloneLoader.load(2);
-//    console.log("yyyy");
-// })()
 
 const resolvers = {
     Trainer: {
         pokemons: async ( {id} ) => {
             // const pokemons = await DbMock.getPokemonsForTrainer(id);
             const pokemons = await cloneLoader.load(id);
-            // const pokemons_ = await realLoader.load(id);
+            // const pokemons = await realLoader.load(id);
             return pokemons;
         },
     },
@@ -62,6 +54,3 @@ server.listen({port: 4001}).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
 });
 
-// https://github.com/graphql/dataloader
-// https://www.google.com/search?q=process+nexttick+vs+setimmediate&oq=process+nexttick&aqs=chrome.1.69i57j0i512j0i22i30l8.3957j0j7&sourceid=chrome&ie=UTF-8
-// https://studio.apollographql.com/sandbox/explorer
