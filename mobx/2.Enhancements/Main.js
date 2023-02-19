99d052ec732fed0edfd550300b96af43cf62ecfa
Nir rose <nirrose@Nirs-MacBook-Pro.local>
2023-02-19 15:12:47 +0200

    mobx clone

--- mobx/2.Enhancements/Main.js
+++ mobx/2.Enhancements/Main.js
@@ -4,24 +4,11 @@ const { Timer } = require('./Timer');
 const myTimer = new Timer()
 const title = { text:"Hi" };
 
-TimerView({timer: myTimer, title});                          
-// console.log(myTimer.secondsPassed, myTimer.format);
-myTimer.tick();                                            
-myTimer.changeFormat('12h');                                          
+TimerView({timer: myTimer, title});                                     // Title: Hi | secondsPassed: 0 | format: 24h              
+myTimer.tick();                                                         // Title: Hi | secondsPassed: 1 | format: 24h  
+myTimer.changeFormat('12h');                                            // Title: Hi | secondsPassed: 1 | format: 12h  
 
-myTimer.tick();         
-myTimer.changeName('mySuperTimer');     // nothing happens                                
-TimerView({timer: myTimer, title: {...title, text:"Bye"} });
-myTimer.tick();                                            
-// console.log(myTimer.secondsPassed, myTimer.format);
-
-/*
-
-Title: Hi | secondsPassed: 0 | format: 24h
-Title: Hi | secondsPassed: 1 | format: 24h
-Title: Hi | secondsPassed: 1 | format: 12h
-Title: Hi | secondsPassed: 2 | format: 12h
-Title: Bye | secondsPassed: 2 | format: 12h
-Title: Bye | secondsPassed: 3 | format: 12h
-
-*/
+myTimer.tick();                                                         // Title: Hi | secondsPassed: 2 | format: 12h  
+myTimer.changeName('mySuperTimer');                                     // no print 
+TimerView({timer: myTimer, title: {...title, text:"Bye"} });            // Title: Bye | secondsPassed: 2 | format: 12h  
+myTimer.tick();                                                         // Title: Bye | secondsPassed: 3 | format: 12h      
